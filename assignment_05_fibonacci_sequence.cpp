50bf07a7cdcafe5f2d5835f3a61eb4edc115fe62
Issah-coder <sanisalamatu679@gmail.com>
2026-08-02 19:56:25 +0000

    Dinw

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -49,5 +49,82 @@
 // =============================================================================
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
+
+vector<int> generate_fibonacci_numbers(int n)
+{
+    vector<int> nums = {0, 1};
+
+    if (n < 0)
+    {
+        return {};
+    }
+
+    if (n == 1)
+    {
+        return {nums[0]};
+    }
+
+    for (int i = 0; i < n - 2; i++)
+    {
+        int fibonacci_number = nums[i] + nums[i + 1];
+        nums.push_back(fibonacci_number);
+    }
+
+    return nums;
+}
+
+bool check_fibonacci_number(int n)
+{
+    vector<int> nums = generate_fibonacci_numbers(n + 1);
+
+    for (int num : nums)
+    {
+        if (num == n)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    int number_of_terms;
+
+    cout << "How many terms? ";
+    cin >> number_of_terms;
+
+    vector<int> fibonacci_numbers = generate_fibonacci_numbers(number_of_terms);
+
+    if (!fibonacci_numbers.empty())
+    {
+        for (int num : fibonacci_numbers)
+        {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << "Invalid Input" << endl;
+    }
+
+    int check_number;
+    cout << "Enter a number to check: ";
+    cin >> check_number;
+
+    if (check_fibonacci_number(check_number))
+    {
+        cout << check_number << " is a fibonacci number" << endl;
+    }
+    else
+    {
+        cout << check_number << " is not a fibonacci number" << endl;
+    }
+
+    return 0;
+}
